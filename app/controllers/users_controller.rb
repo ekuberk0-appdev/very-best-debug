0eb676f4c778da837e04189aac3b8348058ce840
edita21 <ekuberk0@chicagobooth.edu>
2021-08-04 23:46:18 +0000

    30% commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
-  def index
+  def all_users
     matching_users = User.all
     @users = matching_users.order(:created_at)
 
-    render({ :template => "users_templates/all_users.html.erb"})
+    render({ :template => "user_templates/all_users.html.erb"})
   end
   
   def show
@@ -32,5 +32,6 @@ class UsersController < ApplicationController
     the_user.save
     redirect_to("/users/#{user.username}")
   end
+
 
 end
